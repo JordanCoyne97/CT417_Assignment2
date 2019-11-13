9d7bc2124b74fc6693c1aed9b53111cc147894fe
JordanCoyne97 <jordancoyne@hotmail.com>
2019-11-13 16:39:14 +0000

    first commit

---
+++ src/main/java/jordan/coyne/App.java
@@ -0,0 +1,13 @@
+package jordan.coyne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
